5df3266ac9d2998d03502f27fd6f923a376faeea
krouu <jueunheo0317@gmail.com>
2025-03-12 12:15:47 +0900

    반응형 적용

--- widgets/main/header/Header.jsx
+++ widgets/main/header/Header.jsx
@@ -1,39 +1,16 @@
-"use client"
-import { motion } from "framer-motion";
-
 const Header = () => {
     return (  
         <section className="w-screen">
             <div style={{position:"relative"}} className="flex justify-center items-center">
                 <img className="w-full" src="/mainBack.gif" alt="" />
                 <div style={{position:"absolute"}} className="flex flex-col items-center">
-                    <motion.div
-                        initial={{ opacity: 0, x:100 }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        viewport={{ once: true, amount : 1 }}
-                        transition={{
-                            duration: 2,
-                            y: { duration: 1 },
-                        }}
+                    <img className="w-fit" src="/mainLogo.png" alt="" />
+                    <h1 
+                        style={{fontFamily:"DungGeunMo"}} 
+                        className="font-black text-white text-4xl md:text-7xl lg:text-9xl"
                     >
-                        <img className="w-fit" src="/mainLogo.png" alt="" />
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0, x:-100 }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        viewport={{ once: true, amount : 1 }}
-                        transition={{
-                            duration: 2,
-                            y: { duration: 1 },
-                        }}
-                    >
-                        <h1 
-                            style={{fontFamily:"DungGeunMo"}} 
-                            className="font-black text-white text-9xl"
-                        >
-                            PORTFOLIO
-                        </h1>
-                    </motion.div>
+                        PORTFOLIO
+                    </h1>
                 </div>
             </div>
         </section>
