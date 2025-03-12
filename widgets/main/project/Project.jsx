2811055f5f15b11a08b3fe60212253b5804380cc
krouu <jueunheo0317@gmail.com>
2025-03-12 15:42:45 +0900

    screen 사이즈 수정

--- widgets/main/project/Project.jsx
+++ widgets/main/project/Project.jsx
@@ -5,7 +5,7 @@ import ProjectCard from "./ui/ProjectCard";
 const Project = () => {
     return (  
         <section className="w-screen">
-            <article className="w-full max-w-screen-xl m-auto py-24">
+            <article className="w-full max-w-md md:max-w-screen-md lg:max-w-screen-xl m-auto py-24">
                 <Title title={"PROJECT"} />
                 <div className="w-full grid grid-cols-2 gap-3">
                     {constants.map((v,i)=>(
