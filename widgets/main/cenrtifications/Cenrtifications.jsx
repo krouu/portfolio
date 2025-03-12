aba547f5621aafd149bbd7634b9c365fe56f306e
krouu <jueunheo0317@gmail.com>
2025-03-12 15:47:19 +0900

    cenrtifications 반응형 적용

--- widgets/main/cenrtifications/Cenrtifications.jsx
+++ widgets/main/cenrtifications/Cenrtifications.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const Cenrtifications = () => {
     return (  
         <section style={{backgroundColor:`${shareConstants.color.beige}`}} className="w-screen">
-            <article className="w-full max-w-screen-xl m-auto py-24 relative">
+            <article className="w-full max-w-md md:max-w-screen-md lg:max-w-screen-xl m-auto py-24 relative">
                 <Title title={"CENRTIFICATIONS"} />
                 <div className="grid grid-cols-3 pb-16">
                     {constants.cenrtifications.map((v,i)=>(
@@ -16,7 +16,7 @@ const Cenrtifications = () => {
                 </div>
                 <Link href={"/mystery"}>
                     <div className="w-fit bg-white p-5 rounded-full absolute right-0 bottom-10 cursor-pointer">
-                        <img className="w-20" src="/mysteryBox.png" alt="" />
+                        <img className="w-10 md:w-20" src="/mysteryBox.png" alt="" />
                     </div>
                 </Link>
             </article>
--- widgets/main/cenrtifications/ui/CenrtContent.jsx
+++ widgets/main/cenrtifications/ui/CenrtContent.jsx
@@ -4,7 +4,9 @@ const CenrtContent = ({name, date, text}) => {
     return (  
         <div className="relative flex justify-center items-center">
             <img src="/leaf.png" alt="" />
-            <div style={{fontFamily:`${shareConstants.font.pixel}`}} className="absolute flex flex-col gap-3 items-center font-bold text-2xl pt-14 pl-6">
+            <div style={{fontFamily:`${shareConstants.font.pixel}`}} 
+                 className="absolute flex flex-col gap-1 md:gap-3 items-center font-bold text-xs md:text-2xl pt-4 md:pt-14 pl-3 md:pl-6"
+            >
                 <p className="text-white">{name}</p>
                 <p className="text-white">{date}</p>
                 <p className="text-green-900">{text}</p>
